79fd45ced71ec0ef2491186444392b903bcf3b33
Matheus Nery <matheusnery345@gmail.com>
2023-06-13 21:33:44 -0400

    [feat] Utilização do axios get para retorno dos dados

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import axios from "axios"
+import { useState, useEffect } from "react"
+
+import './Home.css'
 
 const Home = () => {
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    try {
+      const response = await axios.get('http://localhost:3000/movies/1/popularity.desc');
+      const data = response.data;      
+      setMovies(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <h1>Top Ten Movies da semana !!</h1>
+      {movies.length === 0 
+        ? (<p>Carregando ...</p>) 
+        : ( 
+          movies.map((movie) => (
+            <div className="movie" key={movie.id}>
+              <h3>{movie.title}</h3>
+            </div>
+          ))
+        )
+      }
+    </div>
+  );
+};
 
 export default Home
